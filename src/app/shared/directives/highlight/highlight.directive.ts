9822660a7c9f05f9121d62b14a215b41594960b8
abel <abellpazz@gmail.com>
2020-12-29 20:50:08 -0500

    Generando matToolbar Implemenando css

--- src/app/shared/directives/highlight/highlight.directive.ts
+++ src/app/shared/directives/highlight/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive ,ElementRef} from '@angular/core';
 export class HighlightDirective {
 
   constructor(elementref:ElementRef) {
-    elementref.nativeElement.style.backgroundColor= 'red';
+    elementref.nativeElement.style.backgroundColor= 'purple';
    }
 
 }
